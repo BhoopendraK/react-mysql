c2fd89208e2579c877a2b09f630e86e6cf7992ca
BhoopendraK <69762030+BhoopendraK@users.noreply.github.com>
2025-01-25 00:03:41 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || PORT
 app.use(cors(
   {
     // origin: "http://localhost:5173",
-// origin: "https://react-mysql-93tt.vercel.app/",
-    origin:"*",
+    origin: "https://react-mysql-93tt.vercel.app/",
+    // origin:"*",
     // CORS_ORIGIN : "*",
   } 
   ))
