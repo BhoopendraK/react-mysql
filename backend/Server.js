7a6030cf8bdec972def74b436fa92297b28e2a3b
BhoopendraK <69762030+BhoopendraK@users.noreply.github.com>
2025-01-25 05:58:07 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -13,6 +13,13 @@ const cors = require("cors")
 const app=express();
 const PORT = process.env.PORT || PORT
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://react-mysql-93tt.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+
 app.use(cors(
   {
     // origin: "http://localhost:5173",
